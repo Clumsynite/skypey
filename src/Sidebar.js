1752b6068bccdac1c3e28263e8ccdbf680e3998c
Clumsy Knight <clusmsyknight@gmail.com>
2021-01-09 16:41:01 +0530

    modified component to render contacts in th sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,14 @@
-const Sidebar = () => {
-  return <aside className="Sidebar">Sidebar</aside>;
+import User from "./User";
+import "./styles/Sidebar.css";
+
+const Sidebar = ({ contacts }) => {
+  return (
+    <aside className="Sidebar">
+      {contacts.map((contact) => (
+        <User user={contact} key={contact.user_id} />
+      ))}
+    </aside>
+  );
 };
 
 export default Sidebar;
