cc22064d222d7ff54f10e5f37bd082c126075e1b
Clumsy Knight <clusmsyknight@gmail.com>
2021-01-09 23:21:24 +0530

    added typing constant, action and reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from "redux";
 
 import user from "./user";
+import typing from "./typing";
 import contacts from "./contacts";
 import messages from "./messages";
 import activeUserId from "./activeUserId";
 
 export default combineReducers({
   user,
+  typing,
   contacts,
   messages,
   activeUserId,
