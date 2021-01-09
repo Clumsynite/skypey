59850a792ecde309fd85adbbd6ecbb8e9ed9daed
Clumsy Knight <clusmsyknight@gmail.com>
2021-01-09 22:47:40 +0530

    created messages reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import user from "./user";
 import contacts from "./contacts";
+import messages from "./messages";
 import activeUserId from "./activeUserId";
 
 export default combineReducers({
   user,
   contacts,
+  messages,
   activeUserId,
 });
