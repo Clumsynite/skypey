52b4722839e5c116cf1f3e3f82b3c92bfa1d051a
Clumsy Knight <clusmsyknight@gmail.com>
2021-01-09 22:47:01 +0530

    added eventHandler for UserClick

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Avatar from "react-avatar";
 
+import store from "../store";
+import { setActiveUserId } from "../actions";
+
 import "./User.css";
 
 const User = ({ user }) => {
   const { name, status } = user;
+
   return (
-    <div className="User">
+    <div className="User" onClick={handleUserClick.bind(null, user)}>
       <Avatar name={name} className="User__pic" size={50} />
       <div className="User__details">
         <p className="User__details-name">{name}</p>
@@ -14,6 +18,10 @@ const User = ({ user }) => {
       </div>
     </div>
   );
+
+  function handleUserClick({ user_id }) {
+    store.dispatch(setActiveUserId(user_id));
+  }
 };
 
 export default User;
